7217faf445c70fd61f4bb4fc7541321acc95b457
clementef <clementef2000@gmail.com>
2024-02-24 14:32:06 -0800

    add float rules for nm and bitwarden

--- config.h
+++ config.h
@@ -28,7 +28,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                        instance    title   tags mask   isfloating  monitor */
-	{ "Nm-connection-editor",       NULL,       NULL,   1,          1,          -1 },
+	{ "Nm-connection-editor",       NULL,       NULL,   0,          1,          -1 },
+	{ "Bitwarden",                  NULL,       NULL,   0,          1,          -1 },
 };
 
 /* layout(s) */
